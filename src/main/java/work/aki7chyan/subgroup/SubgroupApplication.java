66937400754b48e8c296794c26976caf948e6867
Aki7chyan <1476911135@qq.com>
2020-07-16 01:46:19 +0800

    创建项目

---
+++ src/main/java/work/aki7chyan/subgroup/SubgroupApplication.java
@@ -0,0 +1,13 @@
+package work.aki7chyan.subgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubgroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/aki7chyan/subgroup/SubgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package work.aki7chyan.subgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
